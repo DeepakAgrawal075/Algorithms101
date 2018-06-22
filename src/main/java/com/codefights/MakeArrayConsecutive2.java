e21488533f18809835f5033eb5eac965c9a033b2
Deepak Agrawal <bits.deepak075@gmail.com>
2018-06-21 21:34:05 -0400

    Package Refactoring!

--- src/main/java/com/codefights/MakeArrayConsecutive2.java
+++ src/main/java/com/codefights/MakeArrayConsecutive2.java
@@ -1,3 +1,5 @@
+package com.codefights;
+
 /**
  * Ratiorg got statues of different sizes as a present from CodeMaster for his birthday, each statue having an non-negative integer size.
  * Since he likes to make things perfect, he wants to arrange them from smallest to largest so that each statue will be bigger than the previous one exactly by 1.
