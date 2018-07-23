549d07e7b7085efd587269673e49a84dd00acbb6
Deepak Agrawal <bits.deepak075@gmail.com>
2018-07-22 22:50:09 -0400

    42. Trapping Rain Water

--- src/main/java/com/leetcode/TrappingRainWater.java
+++ src/main/java/com/leetcode/TrappingRainWater.java
@@ -18,14 +18,14 @@ class TrappingRainWater {
         int leftMax = 0, rightMax = 0;
         while (left < right) {
             if (height[left] < height[right]) {
-                if (height[left] >= leftMax) {
+                if (height[left] > leftMax) {
                     leftMax = height[left];
                 } else {
                     result += (leftMax - height[left]);
                 }
                 ++left;
             } else {
-                if (height[right] >= rightMax) {
+                if (height[right] > rightMax) {
                     rightMax = height[right];
                 } else {
                     result += (rightMax - height[right]);
