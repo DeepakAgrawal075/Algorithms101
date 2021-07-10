1b1e507337490c71139e3ecff89fec6885e046c2
Deepak Agrawal <bits.deepak075@gmail.com>
2021-07-10 18:46:46 -0400

    Enhancing Test Case

--- src/test/java/net/projecteuler/MultiplesOfThreeAndFiveTest.java
+++ src/test/java/net/projecteuler/MultiplesOfThreeAndFiveTest.java
@@ -8,6 +8,7 @@ class MultiplesOfThreeAndFiveTest {
     @Test
     void givenRangeReturnSumOfMultiplesOfThreeAndFive() {
         assertEquals(23, MultiplesOfThreeAndFive.multiplesOf3And5(10));
+        assertEquals(3, MultiplesOfThreeAndFive.multiplesOf3And5(5));
         assertEquals(233168, MultiplesOfThreeAndFive.multiplesOf3And5(1000));
     }
 }
