c0e748450906655e569201163d090e35b6cb98c9
Deepak Agrawal <bits.deepak075@gmail.com>
2021-07-12 21:20:19 -0400

    All Permutations

---
+++ src/main/java/AllPermutations.java
@@ -0,0 +1,24 @@
+/*
+ * I/P: ABC
+ * O/P: ABC, ACB, BAC, BCA, CAB, CBA
+ */
+import java.util.Scanner;
+
+public class AllPermutations {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String suffix = input.next();
+        showPattern("", suffix);
+    }
+
+    private static void showPattern(String prefix, String suffix) {
+        if (suffix.length() <= 1) {
+            System.out.println(prefix + suffix);
+        } else {
+            for (int i = 0; i < suffix.length(); i++) {
+                String newString = suffix.substring(0, i) + suffix.substring(i + 1);
+                showPattern(prefix + suffix.charAt(i), newString);
+            }
+        }
+    }
+}
