9f6d3355a3a9de4119f9429d1d00e9831e31d539
Deepak Agrawal <bits.deepak075@gmail.com>
2021-08-28 04:19:59 -0400

    136. Single Number

---
+++ src/main/java/com/leetcode/SingleNumber.java
@@ -0,0 +1,34 @@
+/**
+ * 136. Single Number
+ * Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+ * You must implement a solution with a linear runtime complexity and use only constant extra space.
+ * A space complexity of O(1) means that the space required by the algorithm to process data is constant;
+ * it does not grow with the size of the data on which the algorithm is operating.
+ */
+package com.leetcode;
+
+public class SingleNumber {
+    public static int singleNumber(int[] nums) {
+        /*HashMap<Integer, Integer> numFreq = new HashMap<>();
+        for (int num : nums) {
+            if (!numFreq.containsKey(num)) {
+                numFreq.put(num, 1);
+            } else {
+                numFreq.put(num, numFreq.get(num) + 1);
+            }
+        }
+        for (Map.Entry<Integer, Integer> entry : numFreq.entrySet()) {
+            int key = entry.getKey();
+            int value = entry.getValue();
+            if (value == 1)
+                return key;
+        }
+        return -1;
+         */
+        int result = 0;
+        for (int num : nums) {
+            result ^= num; //XOR
+        }
+        return result;
+    }
+}
---
+++ src/test/java/com/leetcode/SingleNumberTest.java
@@ -0,0 +1,15 @@
+package com.leetcode;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SingleNumberTest {
+
+    @Test
+    void givenArrayReturnSingleNumber() {
+        assertEquals(1, SingleNumber.singleNumber(new int[]{2, 2, 1}));
+        assertEquals(4, SingleNumber.singleNumber(new int[]{4, 1, 2, 1, 2}));
+        assertEquals(1, SingleNumber.singleNumber(new int[]{1}));
+    }
+}
