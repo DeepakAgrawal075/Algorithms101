76350f45208784a6f648ebfcd8152ebaf4f4a653
Deepak Agrawal <bits.deepak075@gmail.com>
2021-08-29 22:29:32 -0400

    79. Word Search

---
+++ src/test/java/com/leetcode/WordSearchTest.java
@@ -0,0 +1,15 @@
+package com.leetcode;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class WordSearchTest {
+    @Test
+    void givenBoardAndWordReturnWordExists() {
+        assertTrue(WordSearch.exist(new char[][]{{'A', 'B', 'C', 'E'}, {'S', 'F', 'C', 'S'}, {'A', 'D', 'E', 'E'}}, "ABCCED"));
+        assertTrue(WordSearch.exist(new char[][]{{'A', 'B', 'C', 'E'}, {'S', 'F', 'C', 'S'}, {'A', 'D', 'E', 'E'}}, "SEE"));
+        assertFalse(WordSearch.exist(new char[][]{{'A', 'B', 'C', 'E'}, {'S', 'F', 'C', 'S'}, {'A', 'D', 'E', 'E'}}, "ABCB"));
+    }
+}
