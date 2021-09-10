f2f6997e81ce5c8bbc7a5780873abcabd72f6b05
Deepak Agrawal <bits.deepak075@gmail.com>
2021-09-10 00:14:08 -0400

    283. Move Zeroes

---
+++ src/main/java/com/leetcode/MoveZeroes.java
@@ -0,0 +1,34 @@
+/**
+ * 283. Move Zeroes
+ * Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+ * <p>
+ * Note that you must do this in-place without making a copy of the array.
+ */
+package com.leetcode;
+
+public class MoveZeroes {
+    public void moveZeroes(int[] nums) {
+        int snowBallSize = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0) {
+                snowBallSize++;
+            } else if (snowBallSize > 0) {
+                nums[i - snowBallSize] = nums[i];
+                nums[i] = 0;
+            }
+        }
+    }
+
+    public void moveZeroesII(int[] nums) {
+        if (nums == null || nums.length == 0) return;
+
+        int insertPos = 0;
+        for (int num : nums) {
+            if (num != 0) nums[insertPos++] = num;
+        }
+
+        while (insertPos < nums.length) {
+            nums[insertPos++] = 0;
+        }
+    }
+}
