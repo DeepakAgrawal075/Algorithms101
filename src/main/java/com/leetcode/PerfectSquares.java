e4d742da9972c0c3b5fda02afabac7f58b0a4a09
Deepak Agrawal <bits.deepak075@gmail.com>
2021-09-22 23:28:24 -0400

    279. Perfect Squares

---
+++ src/main/java/com/leetcode/PerfectSquares.java
@@ -0,0 +1,20 @@
+/**
+ * 279. Perfect Squares
+ * Given an integer n, return the least number of perfect square numbers that sum to n.
+ * A perfect square is an integer that is the square of an integer; in other words, it is the product of some integer with itself.
+ * For example, 1, 4, 9, and 16 are perfect squares while 3 and 11 are not.
+ */
+package com.leetcode;
+
+public class PerfectSquares {
+    public int numSquares(int n) {
+        int[] dp = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            dp[i] = i;
+            for (int j = 1; j * j <= i; j++) {
+                dp[i] = Math.min(dp[i], 1 + dp[i - j * j]);
+            }
+        }
+        return dp[n];
+    }
+}
