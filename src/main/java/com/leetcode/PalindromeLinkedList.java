32face90b757558a617d4b2e835842bc35af7be8
Deepak Agrawal <bits.deepak075@gmail.com>
2021-10-06 21:55:29 -0400

    234. Palindrome Linked List

---
+++ src/main/java/com/leetcode/PalindromeLinkedList.java
@@ -0,0 +1,55 @@
+/**
+ * 234. Palindrome Linked List
+ * Given the head of a singly linked list, return true if it is a palindrome.
+ */
+package com.leetcode;
+
+public class PalindromeLinkedList {
+    public boolean isPalindrome(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        slow = reverseRightHalf(slow);
+        fast = head;
+
+        while (slow != null) {
+            if (slow.val != fast.val) {
+                return false;
+            }
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return true;
+    }
+
+    private ListNode reverseRightHalf(ListNode head) {
+        ListNode prev = null;
+        while (head != null) {
+            ListNode next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+        }
+        return prev;
+    }
+
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
