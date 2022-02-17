d291de7a706c3c00b24b1858aa3c0d92d7c2b39b
Deepak Agrawal <bits.deepak075@gmail.com>
2022-02-17 05:52:20 -0500

    Created new package tree

--- src/main/java/com/leetcode/tree/PathSum.java
+++ src/main/java/com/leetcode/tree/PathSum.java
@@ -3,7 +3,7 @@
  * Given the root of a binary tree and an integer targetSum, return true if the tree has a root-to-leaf path such that adding up all the values along the path equals targetSum.
  * A leaf is a node with no children.
  */
-package com.leetcode;
+package com.leetcode.tree;
 
 public class PathSum {
     public static boolean hasPathSum(TreeNode root, int targetSum) {
