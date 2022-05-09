6822e165c85f0e78d28d4cd811738b90b6872729
Deepak Agrawal <bits.deepak075@gmail.com>
2022-05-09 16:25:18 -0400

    326. Power of Three

---
+++ src/main/java/com/leetcode/PowerOfThree.java
@@ -0,0 +1,16 @@
+/**
+ * 326. Power of Three
+ */
+package com.leetcode;
+
+public class PowerOfThree {
+    public boolean isPowerOfThree(int n) {
+        while (n >= 3) {
+            if (n % 3 != 0) {
+                return false;
+            }
+            n = n / 3;
+        }
+        return n == 1;
+    }
+}
