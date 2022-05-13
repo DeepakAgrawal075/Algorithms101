899a3736b1b73cb06e101c0b12aa1e48a0db05cd
Deepak Agrawal <bits.deepak075@gmail.com>
2022-05-13 18:03:28 -0400

    TreeUtils Reformat

--- src/main/java/com/leetcode/tree/TreeUtils.java
+++ src/main/java/com/leetcode/tree/TreeUtils.java
@@ -19,3 +19,28 @@ public class TreeUtils {
         }
     }
 }
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class Tree {
+    TreeNode root;
+}
