cb97ed007537a0a8166250c695039154d4815111
Deepak Agrawal <bits.deepak075@gmail.com>
2022-10-24 16:23:34 -0400

    1239. Maximum Length of a Concatenated String with Unique Characters

---
+++ src/main/java/com/leetcode/MaxLength.java
@@ -0,0 +1,41 @@
+/**
+ * 1239. Maximum Length of a Concatenated String with Unique Characters
+ */
+package com.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MaxLength {
+    public int maxLength(List<String> arr) {
+        List<String> ansList = new ArrayList<>();
+        int ans = 0;
+        ansList.add("");
+        for (String str : arr) {
+            if (!checkUniqueCharString(str)) continue;
+            List<String> newAnsListWithCurrStr = new ArrayList<>();
+            for (String candidate : ansList) {
+                String temp = candidate + str;
+                if (checkUniqueCharString(temp)) {
+                    newAnsListWithCurrStr.add(temp);
+                    ans = Math.max(ans, temp.length());
+                }
+            }
+            ansList.addAll(newAnsListWithCurrStr);
+        }
+        return ans;
+    }
+
+    private boolean checkUniqueCharString(String str) {
+        if (str.length() > 26) return false;
+        int[] freq = new int[26];
+        char[] arr = str.toCharArray();
+        for (char ch : arr) {
+            freq[ch - 'a']++;
+            if (freq[ch - 'a'] > 1) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
