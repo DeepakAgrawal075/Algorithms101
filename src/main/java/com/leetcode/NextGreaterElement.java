d59a7f339c0b6a66e69c6cadf145fadeef8bfaad
Deepak Agrawal <bits.deepak075@gmail.com>
2024-02-03 16:51:55 -0500

    496 : Next Greater Element I

---
+++ src/main/java/com/leetcode/NextGreaterElement.java
@@ -0,0 +1,25 @@
+/**
+ * 496 : Next Greater Element I
+ */
+package com.leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+public class NextGreaterElement {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        Map<Integer, Integer> elNextGreaterEl = new HashMap<>();
+        Stack<Integer> stack = new Stack<>();
+        for (int num : nums2) {
+            while (!stack.empty() && stack.peek() < num) {
+                elNextGreaterEl.put(stack.pop(), num);
+            }
+            stack.push(num);
+        }
+        for (int i = 0; i < nums1.length; i++) {
+            nums1[i] = elNextGreaterEl.getOrDefault(nums1[i], -1);
+        }
+        return nums1;
+    }
+}
