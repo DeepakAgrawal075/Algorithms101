bbb71ef4d3076e4c5e3970025b5ca196f1ead3cc
Deepak Agrawal <bits.deepak075@gmail.com>
2024-08-03 07:41:49 -0400

    GitHub Copilot Simplify

--- src/main/java/com/leetcode/SearchInsertPosition.java
+++ src/main/java/com/leetcode/SearchInsertPosition.java
@@ -8,11 +8,9 @@ package com.leetcode;
 
 public class SearchInsertPosition {
     public int searchInsert(int[] nums, int target) {
-        int mid;
-        int left = 0;
-        int right = nums.length - 1;
+        int left = 0, right = nums.length - 1;
         while (left <= right) {
-            mid = left + (right - left) / 2; // Reduces the risk of integer overflow
+            int mid = left + (right - left) / 2;
             if (nums[mid] == target) {
                 return mid;
             } else if (nums[mid] > target) {
